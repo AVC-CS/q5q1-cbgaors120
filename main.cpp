4059b2801a7df50addc474dd0994af955b42ba80
cbgaors120 <bgaor@students.avc.edu>
2026-04-13 13:02:44 -0700

    have infinite loop issues, debugging.

--- main.cpp
+++ main.cpp
@@ -4,11 +4,27 @@ using namespace std;
 int main()
 {
     int n;
+    char letter = 'A';
+
     // TODO: ask user for input
+    cout << "Enter number" << endl;
+    cin >> n;
 
     // TODO: outer loop for each row
+    for (int i=0;i<n;i++){
 
-        // TODO: inner loop to print letters for this row
+        for (int j=0;j<i;i++){
+            cout << letter << " ";
+            letter++;
+        }
+
+        cout << endl;
+        
+    }
+
+    
+        // TODO: inner loop to print letters for this row .. char 65-69 ascii
+            
 
             // TODO: print the letter for this column
 
