0608775da88624e3352b663e5030941da35a39f3
cbgaors120 <bgaor@students.avc.edu>
2026-04-13 13:12:55 -0700

    not getting the correct loop.

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ int main()
     // TODO: outer loop for each row
     for (int i=0;i<n;i++){
 
-        for (int j=0;j<i;i++){
+        for (int j=0;j<=i;j++){
             cout << letter << " ";
             letter++;
         }
